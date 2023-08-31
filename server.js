ccf47f950ec455c9b6855b5eb2f6b7edcebb3cdf
santajoyal <santajoyal07@gmail.com>
2023-08-31 14:49:06 +0530

    final change

--- server.js
+++ server.js
@@ -35,3 +35,6 @@ app.get("/getpizzas",(req,res)=>{
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => `Server running on port🔥`)
+
+LINK="mongodb+srv://admin:admin123@cluster0.ymf03hp.mongodb.net/mern-pizza"
+STRIPE ="sk_test_51Nk3VISGRlunq0a06Nhr9Eql9GE1cGZSaMT1SZhoNl2xpRiPwNBIWQ0Q8Tdhs2JEfNCaVJwB7msgfV5fVmiBEzwe00yuC3HvfP"
